28c69f507f032d12c355d11e370ece3e05ea3942
CasShandu <cas.shandu@gmail.com>
2024-10-10 11:25:14 +0200

    string concantenation

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""module that creates a function that concatenates two strings."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings together
+
+    Args:
+        str1 (str): First string.
+        str2 (str): Second string.
+
+    Returns:
+        str: The concatenated string.
+    """
+    return str1 + str2
