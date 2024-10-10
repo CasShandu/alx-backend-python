97e374fd52c4b276d7a3a9fe68a1094339eef2c9
CasShandu <cas.shandu@gmail.com>
2024-10-10 11:32:06 +0200

    string representation of float number

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""Returns the string representation of a float."""
+
+
+def to_str(n: float) -> str:
+    """
+    Returns the string representation of a float.
+
+    Args:
+        n: Represents a float number.
+
+    Returns:
+        str: The string representation of the float number.
+    """
+    return str(n)
