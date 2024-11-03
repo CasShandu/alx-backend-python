1cd4ea7dc3396a8d20644486947726418f1c77ec
“CasShandu” <cas.shandu@gmail.com>
2024-11-03 12:55:15 +0200

    test utils task 0

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,18 +1,23 @@
-#!/usr/bin/python3
-
+#!/usr/bin/env python3
+"""Parameterize a unit test
+"""
 import unittest
 from utils import access_nested_map
 from parameterized import parameterized
 
+
 class TestAccessNestedMap(unittest.TestCase):
+    """Test access_nested_map utils methods
+    """
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nested_map, path, expected):
-        self.assertEqual(access_nested_map(nested_map, path), expected)
+        """Test that the method returns what it is supposed to."""
+        result = access_nested_map(nested_map, path)
+        self.assertEqual(result, expected)
 
 if __name__ == "__main__":
-    unittest.main()
-
+    unittest.main(verbosity=2)
--- 0x03-Unittests_and_integration_tests/utils.py
+++ 0x03-Unittests_and_integration_tests/utils.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def access_nested_map(nested_map, path):
     for key in path:
         nested_map = nested_map[key]
